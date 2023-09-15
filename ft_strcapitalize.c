3f6fef9cbd6898732d2f0e9495f9f359a8cc0c66
fufulouteur <manzodqb@icloud.com>
2023-09-15 07:14:51 +0000

    fini

--- ft_strcapitalize.c
+++ ft_strcapitalize.c
@@ -4,13 +4,19 @@ char *ft_strcapitalize(char *str) {
   char *str2;
 
   str2 = str;
-  while (str) {
-    if (*str >= ' ' && *str <= '?') {
+  while (*str != '\0') {
+    if (*str == '?')
       str++;
-      if (*str <= 'a' && *str >= 'z') {
-        *str -= 32;
+    if (*str >= 'a' && *str <= 'z')
+      *str -= 32;
+    if (*str >= ' ' || *str <= '?')
+      while (*str != ' ') {
+        str++;
+        if (*str == '\0') {
+          break;
+        }
       }
-    }
+
     str++;
   }
   return str2;
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 char *ft_strcapitalize(char *str);
 
 int main() {
-  char tab[] = "salut, comment sa va ?";
-  ft_strcapitalize(tab);
+  char tab[] = "salut, comment tu vas. 42mots quarante-deux; cinquante+et+un";
+  printf("%s\n", ft_strcapitalize(tab));
   return 0;
 }
